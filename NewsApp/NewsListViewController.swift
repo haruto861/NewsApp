61f8ac2c6f38e1e517cf60906156251de6429774
haruto-matsushima <matsu_haruto@icloud.com>
2021-11-09 23:28:21 +0900

    add rx, delete main.storyboard, viewcontroller

---
+++ NewsApp/NewsListViewController.swift
@@ -0,0 +1,11 @@
+//
+//  NewsListViewController.swift
+//  NewsApp
+//
+//  Created by 松島悠人 on 2021/11/09.
+//
+
+import Foundation
+import UIKit
+
+final class NewsListViewController: UIViewController {}
