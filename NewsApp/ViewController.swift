f9100c5754c399e919a9dc71480b5733e9bf5a3b
haruto-matsushima <matsu_haruto@icloud.com>
2021-11-09 23:21:41 +0900

    initial commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by 松島悠人 on 2021/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
